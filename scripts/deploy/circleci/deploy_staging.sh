13481c2ca258523d1dc82b647ebce8dd35cb4e1b
Brock Fanning <brockfanning@gmail.com>
2018-09-15 21:17:36 -0400

    Dynamic repo name in deploy script.

--- scripts/deploy/circleci/deploy_staging.sh
+++ scripts/deploy/circleci/deploy_staging.sh
@@ -7,7 +7,7 @@ git config --global user.name $GH_NAME
 # "Host" (eg, github.com) and so they can't be distinguished. This script assumes
 # they will be named "gh-stg" and adds the "github.com" as a HostName setting.
 sed -i -e 's/Host gh-stg/Host gh-stg\n  HostName github.com/g' ~/.ssh/config
-git clone git@gh-stg:$GH_ORG_STG/sdg-data.git out
+git clone git@gh-stg:$GH_ORG_STG/$CIRCLE_PROJECT_REPONAME.git out
 
 cd out
 git checkout gh-pages || git checkout --orphan gh-pages
